5a43c5779bc7c9a6662adc1b9f9449d04e4537ad
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-10-23 11:39:42 +0700

    конструкторы классов

--- Prototype/Prototype/Animal.cs
+++ Prototype/Prototype/Animal.cs
@@ -10,11 +10,20 @@ namespace Prototype
     /// Базовый класс Животное
     /// общие данные (id, имя, возраст)
     /// </summary>
-    public class Animal
+    public class Animal : IMyCloneable<Animal>
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Age { get; set; }
+
+        public Animal() { }
+
+        public Animal(int id, string name, int age)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
 
 
     }
--- Prototype/Prototype/Dog.cs
+++ Prototype/Prototype/Dog.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 
 namespace Prototype
 {
+    /// <summary>
+    /// класс Собака
+    /// потомок класса Animal
+    /// добавляет свойство - порода
+    /// </summary>
     public class Dog : Mammal
     {
         public string Breed { get; set; }
+        
+        public Dog() { }
 
+        public Dog(int id, string name, int age, double bodyTemperature, string breed)
+        : base(id, name, age, bodyTemperature) 
+        {
+            Breed = breed;
+        }
     }
 }
