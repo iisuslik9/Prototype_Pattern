bc7c32ed4d11608a322114b7fa4695b68cd87123
vershinina618 <73005698+vershinina618@users.noreply.github.com>
2025-10-23 09:39:39 +0700

    create classes

---
+++ Prototype/Prototype/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ Prototype/Prototype/Fish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public enum WaterType
+    {
+        Freshwater,  
+        Saltwater,   
+    }
+    public class Fish : Animal
+    {
+        public WaterType WaterType { get; set; }
+
+    }
+}
